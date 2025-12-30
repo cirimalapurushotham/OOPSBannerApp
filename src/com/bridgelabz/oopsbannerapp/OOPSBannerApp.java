c63a85d7a70477637ebcccb2fa516ac6312034d5
cirimalapurushotham <purushothamsirimala22@gmail.com>
2025-12-30 16:10:34 +0530

    Print OOPS in console

---
+++ src/com/bridgelabz/oopsbannerapp/OOPSBannerApp.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.oopsbannerapp;
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+    	
+    	        String text = "OOPS";
+    	        System.out.println(text);
+    	    }
+    }
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module OOPS_Banner_App {
+}
